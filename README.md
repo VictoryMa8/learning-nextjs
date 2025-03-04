f6bc2ebbcf8fa5b9d6db9e594389a08041928cad
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-03-04 16:47:07 -0600

    new files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # learning-nextjs
+
+This is where I will be uploading my journey in learning React and Next.js
---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import LikeButton from './like-button';
+
+function Header(properties) {
+    return <h1>{properties.text ? properties.text : "Default Message"}</h1>;
+}
+
+export default function HomePage() {
+    const names = ['Vro', 'Cro', 'Po'];
+    
+    return (
+        <div>
+            <Header text="Hello vro.." />
+            <Header text="Nice to meet you vro..." />
+            <ul>
+                {names.map((i) => (
+                    <li key={i}>{i}</li>
+                ))}
+            </ul>
+            <LikeButton />
+        </div>
+    );
+}
